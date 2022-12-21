cfdabd776698e60544f4bcc9a62096299aca2840
gerlangiosoares <gerlangio@gmail.com>
2022-12-21 16:16:14 -0300

    Update script.js

--- 1_var_let_const/js/script.js
+++ 1_var_let_const/js/script.js
@@ -1,6 +1,32 @@
-var nome = 'Luiz';
+/**
+ * var nome = 'Luiz';
+ */
+let nome = 'Pedro';
+
+/**
+ * let nome = 'Rodrigo';
+ * 
+ * Uncaught Syntax Error: Identifier 'nome' has already been declared
+ * Erro de sintaxe não identificado: o identificador 'nome' já foi declarado
+ */
 
 function dizerNome(){
     var nome = 'João';
     console.log("O nome é " + nome);
+}
+/**
+ * if(true){
+    var nome = 'Salete';
+} 
+ */
+if(true){
+    let nome = 'Salete';
+}
+
+console.log(nome);
+dizerNome();
+
+for(let i = 0; i < 3; i++){
+    let nome = 'Anísio';
+    console.log(nome);
 }
